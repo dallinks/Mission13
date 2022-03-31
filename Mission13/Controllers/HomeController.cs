5c5e5686a61f0113c72d11b2e26af64bb460d3b4
dallinks <dallinstirling@gmail.com>
2022-03-30 23:35:37 -0600

    Add project files.

---
+++ Mission13/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBowlerRepository _repo { get; set; }
+        public HomeController(IBowlerRepository temp)
+        {   
+            _repo = temp;
+        }
+        public IActionResult Index()
+        {
+            var bowlers = _repo.Bowlers.ToList();
+            return View(bowlers);
+        }
+
+    }
+}
---
+++ Mission13/Models/EFBowlerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Models
+{
+    public class EFBowlerRepository : IBowlerRepository
+    {
+        public BowlersDBContext _context {get;set;}
+        public EFBowlerRepository(BowlersDBContext temp)
+        {
+            _context = temp;
+        }
+        public IQueryable<Bowler> Bowlers => _context.Bowlers;
+    }
+}
